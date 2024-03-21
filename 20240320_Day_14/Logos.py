d3ba7e230f83d1639f3aef475d2c48bcc92e477a
mish81 <mish.sam1993@gmail.com>
2024-03-21 06:24:20 +0530

    Updating data from Day 14

--- 20240320_Day_14/Logos.py
+++ 20240320_Day_14/Logos.py
@@ -1,19 +1,70 @@
-logo = """
-    __  ___       __             
-   / / / (_)___ _/ /_  ___  _____
-  / /_/ / / __ `/ __ \/ _ \/ ___/
- / __  / / /_/ / / / /  __/ /    
-/_/ ///_/\__, /_/ /_/\___/_/     
-   / /  /____/_      _____  _____
-  / /   / __ \ | /| / / _ \/ ___/
- / /___/ /_/ / |/ |/ /  __/ /    
-/_____/\____/|__/|__/\___/_/     
-"""
-
-vs = """
- _    __    
-| |  / /____
-| | / / ___/
-| |/ (__  ) 
-|___/____(_)
-"""
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "<>:1: SyntaxWarning: invalid escape sequence '\\/'\n",
+      "<>:1: SyntaxWarning: invalid escape sequence '\\/'\n",
+      "C:\\Users\\kumar\\AppData\\Local\\Temp\\ipykernel_22056\\2965177763.py:1: SyntaxWarning: invalid escape sequence '\\/'\n",
+      "  logo = \"\"\"\n"
+     ]
+    }
+   ],
+   "source": [
+    "logo = \"\"\"\n",
+    "    __  ___       __             \n",
+    "   / / / (_)___ _/ /_  ___  _____\n",
+    "  / /_/ / / __ `/ __ \\/ _ \\/ ___/\n",
+    " / __  / / /_/ / / / /  __/ /    \n",
+    "/_/ ///_/\\__, /_/ /_/\\___/_/     \n",
+    "   / /  /____/_      _____  _____\n",
+    "  / /   / __ \\ | /| / / _ \\/ ___/\n",
+    " / /___/ /_/ / |/ |/ /  __/ /    \n",
+    "/_____/\\____/|__/|__/\\___/_/     \n",
+    "\"\"\"\n",
+    "\n",
+    "vs = \"\"\"\n",
+    " _    __    \n",
+    "| |  / /____\n",
+    "| | / / ___/\n",
+    "| |/ (__  ) \n",
+    "|___/____(_)\n",
+    "\"\"\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
